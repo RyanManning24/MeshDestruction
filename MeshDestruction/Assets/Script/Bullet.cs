14f9717b6f4abe96a1ab29980ac400419ae1e881
Ryan <ryan2.manning@live.uwe.ac.uk>
2022-06-18 21:53:52 +0100

    trying to get a slice working

--- MeshDestruction/Assets/Script/Bullet.cs
+++ MeshDestruction/Assets/Script/Bullet.cs
@@ -6,14 +6,72 @@ public class Bullet : MonoBehaviour
 {
 
     [SerializeField] private SphereCollider sphereCollider;
+    [SerializeField] private bool horizontalCut = false;
+
 
 
     private void OnTriggerEnter(Collider other)
     {
-        //apply Destruction
-        if (other.gameObject.layer == 10)
+        //apply destruction
+        if(horizontalCut)
         {
-            voronoiDestruction.SimpleCut(other.gameObject.transform, other.gameObject.transform.position);
+            //apply everything but horizontal
+            //get impact point
+            //create the start and end points 
         }
+        else if(horizontalCut)
+        {
+            //apply cutting vertical
+        }
+
+        /*_triggerExitTipPosition = _tip.transform.position;
+
+        //Create a triangle between the tip and base so that we can get the normal
+        Vector3 side1 = _triggerExitTipPosition - _triggerEnterTipPosition;
+        Vector3 side2 = _triggerExitTipPosition - _triggerEnterBasePosition;
+
+        //Get the point perpendicular to the triangle above which is the normal
+        Vector3 normal = Vector3.Cross(side1, side2).normalized;
+
+        //Transform the normal so that it is aligned with the object we are slicing's transform.
+        Vector3 transformedNormal = ((Vector3)(other.gameObject.transform.localToWorldMatrix.transpose * normal)).normalized;
+
+        //Get the enter position relative to the object we're cutting's local transform
+        Vector3 transformedStartingPoint = other.gameObject.transform.InverseTransformPoint(_triggerEnterTipPosition);
+
+        Plane plane = new Plane();
+
+        plane.SetNormalAndPosition(
+                transformedNormal,
+                transformedStartingPoint);
+
+        var direction = Vector3.Dot(Vector3.up, transformedNormal);
+
+        //Flip the plane so that we always know which side the positive mesh is on
+        if (direction < 0)
+        {
+            plane = plane.flipped;
+        }*/
+        //create plane 
+        Plane plane = new Plane();
+
+        if(other.TryGetComponent<Renderer>(out Renderer objectHit))
+        {
+            Vector3 planeYZ = new Vector3(0,objectHit.bounds.size.y, objectHit.bounds.size.z);
+
+            //Vector3 side1 = ;
+            //Vector3 side2 = ;
+
+            //Vector3 planeNormal = Vector3.Cross()
+
+            //plane.SetNormalAndPosition();
+        }
+
+        GameObject[] slices = Slicer.Slice(plane, other.gameObject);
+        Destroy(other.gameObject);
+
+        //Rigidbody rigidbody = slices[1].GetComponent<Rigidbody>();
+        //Vector3 newNormal = transformedNormal + Vector3.up * _forceAppliedToCut;
+        //rigidbody.AddForce(newNormal, ForceMode.Impulse);
     }
 }
