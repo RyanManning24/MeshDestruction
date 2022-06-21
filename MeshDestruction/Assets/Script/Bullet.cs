b76f8a720cd07160d35d155e4ee569836a2bc68a
Ryan <ryan2.manning@live.uwe.ac.uk>
2022-06-21 20:35:50 +0100

    Added test material to test cutting with materials

--- MeshDestruction/Assets/Script/Bullet.cs
+++ MeshDestruction/Assets/Script/Bullet.cs
@@ -13,22 +13,13 @@ public class Bullet : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        //doesnt work
         //apply destruction
         if (horizontalCut && collision.gameObject.GetComponent<Sliceable>() && isActive)
         {
             if (collision.gameObject.TryGetComponent<Renderer>(out Renderer objectHit))
-                isActive = false;
-            //apply everything but horizontal
-            //get impact point
-            //create the start and end points 
-        }
-        else if (!horizontalCut && collision.gameObject.GetComponent<Sliceable>() && isActive)
-        {
-            //apply cutting vertical
-            if (collision.gameObject.TryGetComponent<Renderer>(out Renderer objectHit))
             {
                 isActive = false;
-
                 Vector3 planeYZ = new Vector3(0, objectHit.bounds.size.y, objectHit.bounds.size.z);
 
                 Vector3 collisionPoint = collision.contacts[0].point;
@@ -48,6 +39,52 @@ public class Bullet : MonoBehaviour
 
                 Vector3 transformedNormal = ((Vector3)(collision.gameObject.transform.localToWorldMatrix.transpose * normal)).normalized;
 
+
+                Plane plane = new Plane();
+                plane.SetNormalAndPosition(transformedNormal, planeYZ);
+
+                var direction = Vector3.Dot(Vector3.up, transformedNormal);
+
+                if (direction < 0)
+                {
+                    plane = plane.flipped;
+                }
+
+                GameObject[] slices = Slicer.Slice(plane, collision.gameObject);
+                Destroy(collision.gameObject);
+
+                Rigidbody rigidbody = slices[1].GetComponent<Rigidbody>();
+                Vector3 newNormal = transformedNormal + Vector3.up * _forceAppliedToCut;
+                rigidbody.AddForce(newNormal, ForceMode.Impulse);
+            }
+            //apply everything but horizontal
+            //get impact point
+            //create the start and end points 
+        }
+        else if (!horizontalCut && collision.gameObject.GetComponent<Sliceable>() && isActive)
+        {
+            //apply cutting vertical
+            if (collision.gameObject.TryGetComponent<Renderer>(out Renderer objectHit))
+            {
+                isActive = false;
+
+                Vector3 planeYZ = new Vector3(0, objectHit.bounds.size.y, objectHit.bounds.size.z);
+
+                Vector3 collisionPoint = collision.contacts[0].point;
+
+                Vector3 vectorA = new Vector3(collisionPoint.x, collisionPoint.y - planeYZ.y, collisionPoint.z);
+
+                float pointB = collisionPoint.y + planeYZ.y;
+                Vector3 vectorB = new Vector3(collisionPoint.x, collisionPoint.y + planeYZ.y, collisionPoint.z);
+
+                Vector3 endVector = new Vector3(collisionPoint.x, collisionPoint.y, collisionPoint.z + planeYZ.z);
+
+                Vector3 side1 = vectorB - vectorA;
+                Vector3 side2 = vectorB - endVector;
+
+                Vector3 normal = Vector3.Cross(side1, side2).normalized;
+
+                Vector3 transformedNormal = ((Vector3)(collision.gameObject.transform.localToWorldMatrix.transpose * normal)).normalized;
 
                 Plane plane = new Plane();
                 plane.SetNormalAndPosition(transformedNormal, planeYZ);
