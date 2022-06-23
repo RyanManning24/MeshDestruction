9688bf626d3bb0fe290e900d01ae2149b69f981a
Ryan <ryan2.manning@live.uwe.ac.uk>
2022-06-23 21:28:55 +0100

    Cutting corners off of objects

--- MeshDestruction/Assets/Script/Bullet.cs
+++ MeshDestruction/Assets/Script/Bullet.cs
@@ -67,31 +67,33 @@ public class Bullet : MonoBehaviour
             if (collision.gameObject.TryGetComponent<Renderer>(out Renderer objectHit))
             {
                 isActive = false;
-
+                //get hit first hit point
                 Vector3 collisionPoint = collision.contacts[0].point;
+                //set up the size of the object hit
                 Vector3 planeYZ = new Vector3(0 , objectHit.bounds.size.y, objectHit.bounds.size.z);
-                //Vector3 hitObjSize = objectHit.bounds.size;
+                Vector3 hitObjSize = objectHit.bounds.size;
                 //Vector3 planeYZ = hitObjSize - collisionPoint;
+                
+                //calculate the two points of the plane that we will cut the object on
+                Vector3 vectorA = new Vector3(hitObjSize.x - collisionPoint.x, collisionPoint.y - planeYZ.y,collisionPoint.z);
+                Vector3 vectorB = new Vector3(hitObjSize.x - collisionPoint.x, collisionPoint.y + planeYZ.y,collisionPoint.z);
 
-                Vector3 vectorA = new Vector3(collisionPoint.x, collisionPoint.y - planeYZ.y, collisionPoint.z);
-
-                float pointB = collisionPoint.y + planeYZ.y;
-                Vector3 vectorB = new Vector3(collisionPoint.x, collisionPoint.y + planeYZ.y, collisionPoint.z);
-
+                //create the side vectors that will be used to calculate the normal
                 Vector3 endVector = new Vector3(collisionPoint.x, collisionPoint.y, collisionPoint.z + planeYZ.z);
 
                 Vector3 side1 = vectorB - vectorA;
                 Vector3 side2 = vectorB - endVector;
-
+                //calculate the normal
                 Vector3 normal = Vector3.Cross(side1, side2).normalized;
 
+                
                 Vector3 transformedNormal = ((Vector3)(collision.gameObject.transform.localToWorldMatrix.transpose * normal)).normalized;
 
                 Plane plane = new Plane();
                 plane.SetNormalAndPosition(transformedNormal, planeYZ);
 
                 var direction = Vector3.Dot(Vector3.up, transformedNormal);
-
+                
                 if (direction < 0)
                 {
                     plane = plane.flipped;
